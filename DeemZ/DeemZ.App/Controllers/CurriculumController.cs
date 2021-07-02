27db2ffb58add493ba5acc21c43f826ea0059d95
Berat-Dzhevdetov <berkoooo@abv.bg>
2021-07-02 23:05:56 +0300

    Added basic view on index page

---
+++ DeemZ/DeemZ.App/Controllers/CurriculumController.cs
@@ -0,0 +1,11 @@
+﻿namespace DeemZ.App.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    public class CurriculumController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
