1fb310e1a723bd12519faab5800379cf4654deb1
Berat-Dzhevdetov <berkoooo@abv.bg>
2021-07-07 15:22:53 +0300

    Improved UE on index page

--- DeemZ/DeemZ.Web/Startup.cs
+++ DeemZ/DeemZ.Web/Startup.cs
@@ -1,12 +1,12 @@
-using DeemZ.Data;
-using DeemZ.Data.Models;
-using DeemZ.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using DeemZ.Data;
+using DeemZ.Data.Models;
+using DeemZ.Services;
 
 namespace DeemZ.Web
 {
--- DeemZ/DeemZ.Web/wwwroot/js/site.js
+++ DeemZ/DeemZ.Web/wwwroot/js/site.js
@@ -1,1 +1,10 @@
-﻿
+﻿const orangeColor = "#FFA000";
+const regalBlueColor = "#234465";
+document.querySelectorAll('li.info-item').forEach(li => {
+    li.addEventListener('mouseover', e => {
+        e.currentTarget.querySelector('span.arrow-right').style.background = regalBlueColor;
+    });
+    li.addEventListener('mouseout', e => {
+        e.currentTarget.querySelector('span.arrow-right').style.background = orangeColor;
+    });
+})
