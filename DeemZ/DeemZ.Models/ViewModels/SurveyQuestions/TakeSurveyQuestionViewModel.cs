e6b8c168eab7ac674ec1375642df33561d148a9e
Berat-Dzhevdetov <berkoooo@abv.bg>
2021-07-10 22:46:33 +0300

    Added basic for surveys

--- DeemZ/DeemZ.Models/ViewModels/SurveyQuestions/TakeSurveyQuestionViewModel.cs
+++ DeemZ/DeemZ.Models/ViewModels/SurveyQuestions/TakeSurveyQuestionViewModel.cs
@@ -4,6 +4,7 @@
     using DeemZ.Models.ViewModels.SurveyAnswers;
     public class TakeSurveyQuestionViewModel
     {
+        public string Id { get; set; }
         public string Name { get; set; }
         public List<TakeSurveyAnswerViewModel> Answers { get; set; }
     }
--- DeemZ/DeemZ.Services/AutoMapperProfiles/SurveyQuestionProfile.cs
+++ DeemZ/DeemZ.Services/AutoMapperProfiles/SurveyQuestionProfile.cs
@@ -3,13 +3,14 @@
     using AutoMapper;
     using DeemZ.Data.Models;
     using DeemZ.Models.ViewModels.SurveyQuestions;
+    using System.Linq;
 
     public class SurveyQuestionProfile : Profile
     {
         public SurveyQuestionProfile()
         {
             CreateMap<SurveyQuestion, TakeSurveyQuestionViewModel>()
-                .ForMember(x => x.Answers, o => o.MapFrom(src => src.Answers))
+                .ForMember(x => x.Answers, o => o.MapFrom(src => src.Answers.OrderBy(x => x.Text)))
                 .ForMember(x => x.Name, o => o.MapFrom(src => src.Question));
         }
     }
