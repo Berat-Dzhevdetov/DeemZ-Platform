16f5f47f87801dab09f94dae122cdda8734f3028
Berat-Dzhevdetov <berkoooo@abv.bg>
2021-07-15 22:06:44 +0300

    Added initial view for admin panel

---
+++ DeemZ/DeemZ.Web/Controllers/AdministrationController.cs
@@ -0,0 +1,12 @@
+﻿namespace DeemZ.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class AdministrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
