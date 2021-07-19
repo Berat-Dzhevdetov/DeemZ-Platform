bc97c44838b734fdd71c07ab63cb22e2fc44d4cb
Berat-Dzhevdetov <berkoooo@abv.bg>
2021-07-19 16:49:41 +0300

    Added responsive design for index and courses in admin panel

--- DeemZ/DeemZ.Web/Controllers/AdministrationController.cs
+++ DeemZ/DeemZ.Web/Controllers/AdministrationController.cs
@@ -39,8 +39,6 @@
 
             var allPages = (int)Math.Ceiling(adminService.GetUserCoursesCount() / (quantity * 1.0));
 
-            var a = adminService.GetUserCoursesCount();
-
             if (page <= 0 || page > allPages) page = 1;
 
             viewModel.Courses = (List<CoursesViewModel>)adminService.GetAllCourses<CoursesViewModel>(page, quantity);
--- DeemZ/DeemZ.Web/wwwroot/js/admin.js
+++ DeemZ/DeemZ.Web/wwwroot/js/admin.js
@@ -1,1 +1,21 @@
-﻿
+﻿document.querySelector('.mobile-dropdown').addEventListener('click', function (e) {
+    $(this).next(".links-holder").first().slideToggle(300);
+    let i = e.currentTarget.querySelector('i');
+    let span = e.currentTarget.querySelector('span');
+    toggleArrow(i);
+    toggleSpanText(span);
+});
+
+function toggleArrow(i) {
+    if (i.classList.contains('fa-arrow-down'))
+        i.classList = "fas fa-arrow-up";
+    else
+        i.classList = "fas fa-arrow-down";
+}
+
+function toggleSpanText(span) {
+    if (span.innerText.trim() === "Show links")
+        span.innerText = "Hide links";
+    else
+        span.innerText = "Show links";
+}
