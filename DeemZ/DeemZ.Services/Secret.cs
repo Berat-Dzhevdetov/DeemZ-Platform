0dda8f5bf17250ac6537325edcbd926d6a8f992e
Berat-Dzhevdetov <berkoooo@abv.bg>
2021-07-30 23:17:03 +0300

    Updated Secret

---
+++ DeemZ/DeemZ.Services/Secret.cs
@@ -0,0 +1,32 @@
+﻿namespace DeemZ.Services
+{
+    using CloudinaryDotNet;
+
+    public static class Secret
+    {
+        public class CloudinarySetup
+        {
+            private const string cloudName = "dlquhyz0t";
+            private const string key = "797481621562733";
+            private const string secretKey = "c6QXlIqUFFcUzPzYdm3QCtfg1f8";
+            private Account account;
+
+            public CloudinarySetup()
+            {
+                this.Account = new Account(cloudName, key, secretKey);
+            }
+
+            public Account Account
+            {
+                get
+                {
+                    return account;
+                }
+                protected set
+                {
+                    this.account = value;
+                }
+            }
+        }
+    }
+}
