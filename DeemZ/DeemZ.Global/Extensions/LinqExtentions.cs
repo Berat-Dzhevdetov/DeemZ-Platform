74767f16742fc8b219ca4de18031eb46adb45a04
Berat-Dzhevdetov <berkoooo@abv.bg>
2021-08-18 14:03:53 +0300

    Fixed paging

--- DeemZ/DeemZ.Global/Extensions/LinqExtentions.cs
+++ DeemZ/DeemZ.Global/Extensions/LinqExtentions.cs
@@ -6,7 +6,7 @@
     public static class LinqExtentions
     {
         public static IEnumerable<T> Paging<T>(this IEnumerable<T> query, int page = 1, int quantity = 10)
-            => query.Skip(page - 1 * quantity)
+            => query.Skip((page - 1) * quantity)
                 .Take(quantity);
     }
 }
