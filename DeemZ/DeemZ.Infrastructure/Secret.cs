0744963d604dd346a438bee77e0e9f053bbbe6dd
Boyan-Apostolov <boian4934@gmail.com>
2021-09-04 11:00:07 +0300

    Add keys file in DeemZ.Infrastructure

---
+++ DeemZ/DeemZ.Infrastructure/Secret.cs
@@ -0,0 +1,39 @@
+﻿namespace DeemZ.Infrastructure
+{
+    using CloudinaryDotNet;
+
+    public static class Secret
+    {
+        public class CloudinarySetup
+        {
+            private const string cloudName = "";
+            private const string key = "";
+            private const string secretKey = "";
+
+            private Account account;
+
+            public CloudinarySetup()
+            {
+                this.Account = new Account(cloudName, key, secretKey);
+            }
+
+            public Account Account
+            {
+                get
+                {
+                    return account;
+                }
+                protected set
+                {
+                    this.account = value;
+                }
+            }
+        }
+
+        public class GoogleRecaptcha
+        {
+            public const string LocalRecaptchaSiteKey = "";
+            public const string LocalRecaptchaSecretKey = "";
+        }
+    }
+}
