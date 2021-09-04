433f67bda6b21fb1deef45a7ee04a484881917c7
Boyan-Apostolov <boian4934@gmail.com>
2021-09-04 10:39:24 +0300

    Fix GoogleRecaptchaAttribute Namespace

--- DeemZ/DeemZ.Infrastructure/Attributes/GoogleReCaptchaValidationAttribute.cs
+++ DeemZ/DeemZ.Infrastructure/Attributes/GoogleReCaptchaValidationAttribute.cs
@@ -9,7 +9,7 @@ using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using DeemZ.Infrastructure;
 
-namespace DeemZ.Services.Attributes
+namespace DeemZ.Infrastructure.Attributes
 {
     public class GoogleReCaptchaValidationAttribute : ValidationAttribute
     {
