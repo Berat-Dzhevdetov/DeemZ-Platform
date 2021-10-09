a303980eb1fddda1e115210d074297846f1a217d
Berat-Dzhevdetov <berkoooo@abv.bg>
2021-10-09 18:49:54 +0300

    Implemented basic popup for options.

--- DeemZ/Componets/Main/index.js
+++ DeemZ/Componets/Main/index.js
@@ -1,12 +1,33 @@
 const userIteration = document.getElementById('user-iteration'),
-    askQuestionInput = document.getElementById('ask-question-input');
+    askQuestionInput = document.getElementById('ask-question-input'),
+    buger = document.getElementById('burger'),
+    modalContainer = document.getElementById('modal-container'),
+    closeModal = document.getElementById('close-modal');
 
-askQuestionInput.addEventListener('focusin', (e) =>{
+askQuestionInput.addEventListener('focusin', (e) => {
     e.currentTarget.style.height = '60px';
     userIteration.style.height = '90px';
 });
 
-askQuestionInput.addEventListener('focusout', (e) =>{
+askQuestionInput.addEventListener('focusout', (e) => {
     e.currentTarget.style.height = '45px';
     userIteration.style.height = '75px';
 });
+
+buger.addEventListener('click', () => {
+    modalContainer.classList.add('show');
+});
+
+closeModal.addEventListener('click', () => {
+    modalContainer.classList.remove('show');
+})
+
+window.addEventListener('keyup', (e) => {
+    let pressedKey = e.key;
+    switch (pressedKey) {
+        case 'Escape':
+            if(modalContainer.classList.contains('show'))
+                modalContainer.classList.remove('show');
+            break;
+    }
+})
