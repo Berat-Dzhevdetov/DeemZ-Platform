4406b6f40f47c8e9a80e984459be0d2a0729c587
Boyan-Apostolov <boian4934@gmail.com>
2021-10-30 19:51:22 +0300

    Add firebase config

--- LiteChat/LiteChat/src/environments/environment.ts
+++ LiteChat/LiteChat/src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'litechat-28ecb',
+    appId: '1:701515091375:web:00ecb18c60e9b27844335a',
+    storageBucket: 'litechat-28ecb.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCinZgL5sf91iEVhbbQ61TN67qigNuywT0',
+    authDomain: 'litechat-28ecb.firebaseapp.com',
+    messagingSenderId: '701515091375',
+    measurementId: 'G-GFR2DHN7T5',
+  },
   production: false,
   API_ENDPOINT: 'https://localhost:5001/api/messages',
 };
