12b83bfeb89d9641fbb21346e6f2fc516e7c3c86
Berat-Dzhevdetov <berkoooo@abv.bg>
2021-12-09 12:15:48 +0200

    Made model with less information about partners #37

---
+++ DeemZ/DeemZ.Models/ViewModels/Partners/PartnerBasicDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DeemZ.Models.ViewModels.Partners
+{
+    using DeemZ.Data;
+
+    public class PartnerBasicDetailsViewModel
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public PartnerTiers Tier { get; set; }
+        public string Url { get; set; }
+    }
+}
